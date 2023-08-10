d6e8f3cab1b5fbf9d852b64b4db11f94c046e1d6
Carlos Gómez <drcgomez@hotmail.com>
2023-08-10 16:15:45 -0500

    Introducción a las directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { TransformVowelsPipe } from './pipes/transform-vowels.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { TransformVowelsPipe } from './pipes/transform-vowels.pipe';
     NavComponent,
     ReversePipe,
     TimeAgoPipe,
-    TransformVowelsPipe
+    TransformVowelsPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
