d2dc268be028bdae49964fd9a3028c5e88db8d27
Carlos Gómez <drcgomez@hotmail.com>
2023-08-10 11:46:51 -0500

    Obteniendo datos de una API

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { Product } from '../../models/product.model';
+import { StoreService } from '../../services/store.service';
+import { ProductsListService } from '../../services/products-list.service';
 
 @Component({
   selector: 'app-products-list',
@@ -11,49 +13,26 @@ export class ProductsListComponent {
 
   myShoppingCart: Product[] = [];
   total = 0;
+  products: Product[] = [];
+  today = new Date();
+  date = new Date(2021, 1, 21);
 
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-    {
-      id: '5',
-      name: 'Casa para perro',
-      price: 34,
-      image: './assets/images/house.jpg'
-    },
-    {
-      id: '6',
-      name: 'Gafas',
-      price: 3434,
-      image: './assets/images/glasses.jpg'
-    }
-  ];
+  constructor(
+    private storeService: StoreService,
+    private productsListService: ProductsListService
+  ) {
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
+  ngOnInit(): void {
+    this.productsListService.getAllProducts()
+      .subscribe(data => {
+        this.products = data;
+      });
+  }
 
   onAddToShoppingCart(product: Product) {
-    console.log(product);
-    this.myShoppingCart.push(product);
-    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
   }
 }
--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsListService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getAllProducts() {
+    return this.httpClient.get<Product[]>('https://fakestoreapi.com/products');
+  }
 }
